913a8a9154b4f7020a1b36cbc60c82c696d7c0ba
Saudat <saudatshehu5@gmail.com>
2022-06-30 14:14:05 -0700

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,8 +5,9 @@
  * main - entry point
  * Return: 1 if success
  */
+
 int main(void)
 {
-		write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
-			return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }

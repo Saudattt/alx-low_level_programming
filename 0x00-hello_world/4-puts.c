56ce1055272128c82282294ee0fcd6bb2402e071
Saudat <saudatshehu5@gmail.com>
2022-06-30 11:53:09 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
+#include <string.h>
 /*
- * main - enty point
- *
+ * main - entry point
  * Return: Always 0
-  */
+ */
 
-int main(void) 
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzel\n");
 	return (0);

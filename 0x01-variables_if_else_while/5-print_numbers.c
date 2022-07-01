9bb9d06f31c6ad178d9af4f0a57151f76f13417a
Saudat <saudatshehu5@gmail.com>
2022-07-01 10:38:21 -0700

    Base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main- numbers of base 10
+ *
+ * Return: 0 if successful
+ *
+ */
+
+int main(void)
+{
+	int var;
+
+	for (var = 0; var < 10; var++)
+		printf("%d", var);
+
+	printf("\n");
+
+	return (0);
+}

160b1effa92c44def9339d3186dbbf5e95d68cdd
Saudat <saudatshehu5@gmail.com>
2022-07-06 03:05:13 -0700

    The 9 time table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,45 +2,42 @@
 
 /**
  * times_table - prints 9 time table
- *
  * Return: void
  */
 
 void times_table(void)
 {
-	int a = 0;
-	int b;
-	int rep;
-	while (a <= 9)
 
+	int row;
+	int column;
+	int product;
+
+	for (row = 0; row <= 9; row++)
 	{
-		b = 0;
-		while (b <= 9)
+		for (column = 0; column <= 9; column++)
 		{
-			rep = a * b;
-			if (b == 0)
-			{
-				_putchar('0' + rep);
-			}
-			else if (rep < 10)
-			{
-				_putchar(' ');
-				_putchar('0' + rep);
-			}
-			else
-			{
-				_putchar('0' + rep / 10);
-				_putchar('0' + rep % 10);
-			}
 
-			if (b < 9)
+			product = (row * column);
+
+			if (column == 0)
+			{
+				_putchar('0' + product);
+			}
+			else if (product <= 9)
 			{
 				_putchar(',');
 				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + product);
 			}
-			b++;
+			else if (product > 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar('0' + (product / 10));
+				_putchar('0' + (product % 10));
+			}
 		}
 		_putchar('\n');
-		a++;
 	}
 }

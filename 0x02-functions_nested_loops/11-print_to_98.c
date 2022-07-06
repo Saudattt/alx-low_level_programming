fc94bb0be9a0ac13d0176fc8b0e71af1d05961e6
Saudat <saudatshehu5@gmail.com>
2022-07-05 23:42:58 -0700

    n - 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,16 +10,15 @@ void print_to_98(int n)
 {
 	while (n < 98)
 	{
-		printf("%i", n);
+		printf("%i, ", n);
 		n++;
 	}
 	while (n > 98)
 	{
-		printf("%i", n);
+		printf("%i, ", n);
 		n--;
 	}
 
 	printf("98");
-
-	putchar("\n");
+	putchar('\n');
 }

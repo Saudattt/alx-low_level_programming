dee0df12353841b3f089ecb2b074fcdd22870b3e
Saudat <saudatshehu5@gmail.com>
2022-07-14 12:39:01 -0700

    Strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncpy - for copying purpose
+ * @dest: Destination
+ * @src: Source
+ * @n: int
+ * Return: string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	strncpy(dest, src, n);
+	return (dest);
+}
